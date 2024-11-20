301257208a1dae3fc8cff71461440ed47bb78de6
Mahendra Reddy <MMahendraReddy@lnw.com>
2024-11-20 22:59:49 +0530

    Adding src code for the blog: exploring configuration changes

---
+++ Exploring Configuration Changes in .NET Framework and .NET Core/New Configuration/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.Extensions.Configuration;
+
+namespace NewConfiguration
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var config = new ConfigurationBuilder()
+                            .AddJsonFile("appsetting.json")
+                            .AddJsonFile("complexSetting.json") // Adding the other json file also to the config
+                            .Build();
+
+            string fName = config["firstName"];
+            string lName = config["lastName"];
+
+            string capital = config["Address:State:Capital"];
+            string galaxy = config["Address:Location:Galaxy"];
+
+            Console.WriteLine($"First Name: {fName}");
+            Console.WriteLine($"Last  Name: {lName}");
+            Console.WriteLine($"Capital   : {capital}");
+            Console.WriteLine($"Galaxy    : {galaxy}");
+
+        }
+    }
+}
---
+++ Exploring Configuration Changes in .NET Framework and .NET Core/Old_Configuration/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Configuration;
+
+namespace Old_Configuration
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string fName = ConfigurationManager.AppSettings["firstName"];
+            string lName = ConfigurationManager.AppSettings["lastName"];
+
+            Console.WriteLine($"First Name: {fName}");
+            Console.WriteLine($"Last  Name: {lName}");
+
+            Console.ReadLine();
+
+        }
+    }
+}
